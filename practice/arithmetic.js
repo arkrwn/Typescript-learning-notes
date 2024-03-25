f154486ceabd2bc935f2f60dd49ef612892af7f1
Arie Kurniawan <hubungi.aja@gmail.com>
2024-03-25 15:32:23 +0700

    Add practice code: arithmetics and operators

---
+++ practice/arithmetic.js
@@ -0,0 +1,10 @@
+var numberX = 10;
+var numberY = 22;
+var numberZ = 3.5;
+var sumXY = numberX + numberY; // Sum of two integers, result is also an integer
+console.log("Sum Result " + sumXY);
+var divideYZ = numberY / numberZ; // Division of two numbers with decimal places,
+console.log("Divide Y and Z, result " + divideYZ);
+var remainderXY = numberX % numberY; // Remainder/modulus operation on division between
+// two integers, the result is always an integer
+console.log("Remainder X and Y, result " + remainderXY);
---
+++ practice/operators.js
@@ -0,0 +1,10 @@
+var booleanResultAnd = true && false; // Logical AND returns TRUE only if both operands are TRUE
+console.log("Logical AND Result " + booleanResultAnd);
+var booleanResultOr = true || false; // Logical OR returns TRUE if either one or both operands are TRUE
+console.log("Logical OR Result " + booleanResultOr);
+var booleanNotTrue = !booleanResultAnd; // Logical NOT inverts the value of a single operand (TRUE becomes FALSE)
+// Ternary Operator - It is
+// a shorter way of writing an IF statement
+var x = 10;
+var y = (x > 5);
+console.log(y);
